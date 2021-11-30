52dddf05574aeeabd6cde2399873ea2d5c2687cb
Kasper Mróz <kaspermroz1234@gmail.com>
2021-11-30 21:35:19 +0100

    remove everything but config files

--- src/components/App/__tests__/index.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Component from '../index';
-
-describe('[Component] App', () => {
-  it('should render', () => {
-    const { container } = render(<Component />);
-    expect(container).toBeDefined();
-  });
-});
--- src/components/App/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => <main>cleanup</main>;
-
-export default App;
