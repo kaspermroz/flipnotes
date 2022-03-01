4b449e7e659d5239a7e2ae42b1abda4313d9435e
Kasper Mróz <kaspermroz1234@gmail.com>
2022-03-01 20:00:10 +0100

    add sqlite db with initial schema, use Prisma as ORM

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { NextPage } from 'next'
+import { PrismaClient, Flipnote } from "@prisma/client"
 
-const Home: NextPage = () => {
-  return (
-    <div>Hello next.js!</div>
-  )
+type HomePageProps = {
+  flipnotes: Flipnote[];
+}
+
+const Home: NextPage<HomePageProps> = ({ flipnotes }) => {
+  console.log(flipnotes);
+  return <div>Hello next.js!</div>;
+};
+
+export const getServerSideProps = async () => {
+  const db = new PrismaClient()
+  const flipnotes = await db.flipnote.findMany()
+
+  return {
+    props: { flipnotes }
+  }
 }
 
 export default Home
