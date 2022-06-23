4e0b709264ca383811fcad0a85886eb15d4edef3
Kasper Mróz <kaspermroz1234@gmail.com>
2022-06-23 19:28:47 +0200

    Index page web

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
-import { Text, Button } from '@chakra-ui/react';
-import { NavLink } from '@remix-run/react'
+import { Text, Button,  Flex, Box, Center } from '@chakra-ui/react';
+import { NavLink} from '@remix-run/react'
 import { Header } from '~/components/header'
 
 export default function Index() {
@@ -13,7 +13,30 @@ export default function Index() {
           <Button colorScheme="teal">Sign up</Button>
         </NavLink>
       </Header>
-      <Text fontSize="2xl">flipnotes</Text>
+      <Flex mt={40} align="stretch" w="full" pl={120}>
+        <Box w="full">
+          <Text fontSize="6xl">
+            <b>All you know in one place.</b>
+          </Text>
+          <Text fontSize="6xl">
+            <b>Build your own knowledge base.</b>
+          </Text>
+          <Text fontSize="6xl">
+            <b>Flip it untill you remember!</b>
+          </Text>
+        </Box>
+        <Box w="full" position="relative">
+          <Box position="absolute" left="20px" top={0} bg="#FFFFF0" w={500} h={500} border="1px solid #F6E05E" borderRadius={25} zIndex={2}>
+            <Center h="full">
+              <Text fontSize="4xl">
+                <b>React.useEffect()</b>
+              </Text>
+            </Center>
+          </Box>
+          <Box position="absolute" left="40px" top="20px" bg="#F0FFF4" w={500} h={500} border="1px solid #68D391" borderRadius={25} zIndex={1} />
+          <Box position="absolute" left="60px" top="40px" bg="#E6FFFA" w={500} h={500} border="1px solid #4FD1C5" borderRadius={25} zIndex={0} />
+        </Box>
+      </Flex>
     </div>
   );
 }
