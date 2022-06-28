298791484cdb392e7534648889ab9ee6c3180a20
Kasper Mróz <kaspermroz1234@gmail.com>
2022-06-28 17:55:17 +0200

    Add missing dep, refactor test

--- app/components/flipnote/Flipnote.test.tsx
+++ app/components/flipnote/Flipnote.test.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Flipnote } from './Flipnote';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Flipnote } from "./Flipnote";
 
 const defaultProps = {
   title: "test title",
-  content: "test content"
-}
+  content: "test content",
+};
 
 describe("<Flipnote />", () => {
   it("should render", () => {
-    const { container } = render(<Flipnote {...defaultProps} />)
-    expect(container).toBeDefined()
-  })
+    const { container } = render(<Flipnote {...defaultProps} />);
+    expect(container).toBeDefined();
+  });
 
   it("should flip after click", async () => {
-    const { container, getByTestId } =render(<Flipnote {...defaultProps} />)
-    expect(container).toBeDefined()
+    const { container } = render(<Flipnote {...defaultProps} />);
+    expect(container).toBeDefined();
 
-    const flipnote = getByTestId("flipnote-container")
-    expect(flipnote).toBeDefined()
+    const flipnote = screen.getByTestId("flipnote-container");
+    expect(flipnote).toBeInTheDocument();
 
-    const flipnoteFront = getByTestId("flipnote-front")
-    expect(flipnoteFront).toBeDefined()
-    expect(flipnoteFront).toHaveTextContent(defaultProps.title)
+    const flipnoteFront = screen.getByTestId("flipnote-front");
+    expect(flipnoteFront).toBeInTheDocument();
+    expect(flipnoteFront).toHaveTextContent(defaultProps.title);
 
-    const flipnoteBack = getByTestId("flipnote-back")
-    expect(flipnoteBack).toBeDefined()
-    expect(flipnoteBack).toHaveTextContent(defaultProps.content)
+    const flipnoteBack = screen.getByTestId("flipnote-back");
+    expect(flipnoteBack).toBeInTheDocument();
+    expect(flipnoteBack).toHaveTextContent(defaultProps.content);
+    expect(container).toMatchSnapshot();
 
-    await flipnote.click()
-    expect(flipnoteFront).toHaveTextContent(defaultProps.title)
-    expect(flipnoteBack).toHaveTextContent(defaultProps.content)
+    await flipnote.click();
+    expect(flipnoteFront).toHaveTextContent(defaultProps.title);
+    expect(flipnoteBack).toHaveTextContent(defaultProps.content);
+    expect(container).toMatchSnapshot();
   });
 });
